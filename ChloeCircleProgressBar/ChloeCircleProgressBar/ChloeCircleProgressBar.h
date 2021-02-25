0dc3b2257b3d9c6795959fc3f6d02bf7ca63e4f9
kimhyebeen <kimhbin@naver.com>
2021-02-25 22:41:48 +0900

    프로젝트 생성 및 gitignore 파일추가

---
+++ ChloeCircleProgressBar/ChloeCircleProgressBar/ChloeCircleProgressBar.h
@@ -0,0 +1,18 @@
+//
+//  ChloeCircleProgressBar.h
+//  ChloeCircleProgressBar
+//
+//  Created by 김혜빈 on 2021/02/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ChloeCircleProgressBar.
+FOUNDATION_EXPORT double ChloeCircleProgressBarVersionNumber;
+
+//! Project version string for ChloeCircleProgressBar.
+FOUNDATION_EXPORT const unsigned char ChloeCircleProgressBarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ChloeCircleProgressBar/PublicHeader.h>
+
+
